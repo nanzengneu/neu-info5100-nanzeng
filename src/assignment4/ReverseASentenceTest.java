a0b5dcd2c4a8c1803de33c8e32ae6b3caa105a2a
Nan Zeng <zeng.na@husky.neu.edu>
2015-10-14 10:00:54 -0700

    Update

---
+++ src/assignment4/ReverseASentenceTest.java
@@ -0,0 +1,16 @@
+package assignment4;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class ReverseASentenceTest {
+
+	@Test
+	public void testReverseASentence() {
+		ReverseASentence test = new ReverseASentence();
+		String result = test.()
+	}
+
+}
